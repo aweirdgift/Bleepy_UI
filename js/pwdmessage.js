ae18341d2f0f3484d20b1f9abceafb3c4c67ece8
reyniel26 <rmescamillas26@gmail.com>
2021-09-30 20:06:48 +0800

    Updated signup.html
    Added pwdmessage script, this script is partnered with pwdmessage div in signup. This will show if the user pass the criteria for creating strong password

---
+++ js/pwdmessage.js
@@ -0,0 +1,19 @@
+//pwdMessage
+$("#pwdMessage").hide();
+$("#pwd").focus(function() {
+    $("#pwdMessage").show();
+});
+$("#pwd").blur(function() {
+    $("#pwdMessage").hide();
+});
+$("#pwd").keyup(function(){
+    var pwd = $("#pwd").val();
+    $('#smallpwd').attr("class", pwd.match(/[a-z]/g) ?"text-success":"text-danger");
+    $('#smallicon').attr("class", pwd.match(/[a-z]/g)?"fa fa-check":"fa fa-times"); 
+    $('#upperpwd').attr("class", pwd.match(/[A-Z]/g) ?"text-success":"text-danger");
+    $('#uppericon').attr("class", pwd.match(/[A-Z]/g)?"fa fa-check":"fa fa-times");
+    $('#numberpwd').attr("class", pwd.match(/[0-9]/g) ?"text-success":"text-danger");
+    $('#numbericon').attr("class", pwd.match(/[0-9]/g)?"fa fa-check":"fa fa-times");
+    $('#minpwd').attr("class", pwd.length < 8 ?"text-danger":"text-success");
+    $('#minicon').attr("class", pwd.length < 8 ?"fa fa-times":"fa fa-check");
+});
